19d9f6331df52546df5b08f07a6b7e44811c8f82
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2023-02-13 16:43:04 +0330

    List Products

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,43 @@
 import Layout from '@/components/Layout';
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Grid,
+    Typography,
+} from '@mui/material';
+import data from '@/utils/data';
 
 export default function Home() {
     return (
         <Layout>
-            <div>
-                <h1>Products</h1>
-                <ul>
-                    <li>Product 1</li>
-                    <li>Product 2</li>
-                    <li>Product 3</li>
-                </ul>
-            </div>
+            <Grid container spacing={3}>
+                {data.products.map((product) => (
+                    <Grid item md={4} key={product.name}>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    image={product.image}
+                                    title={product.name}
+                                ></CardMedia>
+                                <CardContent>
+                                    <Typography>{product.name}</Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Typography>${product.price}</Typography>
+                                <Button size="small" color="primary">
+                                    Add to cart
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </Layout>
     );
 }
